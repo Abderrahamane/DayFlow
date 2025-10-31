335a4ef9d0caffc13e820e30b50bd3e47a49e224
Abderrahamane <abderrahmane.houri@ensia.edu.dz>
2025-10-31 15:43:02 +0100

    Use platform-specific Firebase options in init
    Updated Firebase initialization to use platform-specific options from DefaultFirebaseOptions. This ensures correct configuration for each platform when starting the app.

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,13 @@ import 'package:dayflow/theme/app_theme.dart';
 import 'utils/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const DayFlowApp());
 }
 
