99d31c0ceb742c817687d2bfca68352b6895e638
Abderrahmane Houri <abderrahmane.houri@ensia.edu.dz>
2025-12-23 15:20:04 +0100

    delete the old project to be able to merge pull request 40

--- lib/pages/onboarding/onboarding.dart
+++
@@ -1,6 +0,0 @@
-// Onboarding Flow Export File
-// Import this file to access all onboarding components
-
-export 'onboarding_page.dart';
-export 'onboarding_model.dart';
-export 'onboarding_slide_widget.dart';
